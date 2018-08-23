c546b80760e5174cfae17e956d5a17149e69b233
xiaoxiaojing1 <736185739@qq.com>
2018-08-23 17:17:55 +0800

    登录退出百度账号

---
+++ test_case/test_baidu_login.py
@@ -0,0 +1,110 @@
+import unittest
+import os
+import time
+import configparser
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+""" Created by xiaoxiaojing on 2018/8/22 """
+
+
+def get_ini_data(section, key):
+    try:
+        project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        ini_path = project_path + "/Conf/config.ini"
+
+        conf = configparser.ConfigParser()
+        conf.read(ini_path, encoding="utf-8")
+        print("即将访问：", conf.get(section, key))
+        return conf.get(section, key)
+    except Exception as err:
+        print(err)
+
+
+class TestBaiduLogin(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("正在初始化……")
+        cls.driver = webdriver.Chrome()
+        cls.driver.get(get_ini_data("Url", "baidu_url"))
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        print("正在执行driver.quit()方法……")
+        cls.driver.quit()
+
+    # 验证是否成功登录，若登录成功，则退出账号
+    def verify_and_logout(self):
+        ele_usrname = self.driver.find_element_by_class_name("user-name")
+        if ele_usrname.text == "22...4@qq.com":
+            print("\n登录成功")
+
+            # 退出登录
+            ele_quit_btn = self.driver.find_element_by_class_name("quit")
+            ActionChains(self.driver).move_to_element(ele_usrname).click(ele_quit_btn).perform()
+            time.sleep(1)
+            self.driver.find_element_by_xpath('//*[@id="tip_con_wrap"]/div[3]/a[3]').click()
+
+            if self.driver.find_element_by_xpath('//*[@id="u1"]/a[7]').text == "登录":
+                print("已退出登录")
+        else:
+            print("登录失败")
+
+    def test_login_with_password(self):
+
+        time.sleep(1)
+        # 进入用户名密码登录页面
+        self.driver.find_element_by_xpath('//*[@id="u1"]/a[7]').click()
+        ele = self.driver.find_element_by_xpath('//*[@id="TANGRAM__PSP_10__qrcode"]/p[1]')
+        print("\n当前弹窗的title为：", ele.text)
+
+        # 验证是否为用户名密码登录
+        if ele.text in "扫码登录":
+            self.driver.find_element_by_id("TANGRAM__PSP_10__footerULoginBtn").click()
+
+        # 输入用户名密码登录
+        self.driver.find_element_by_id("TANGRAM__PSP_10__userName").send_keys("zhanghao")
+        self.driver.find_element_by_id("TANGRAM__PSP_10__password").send_keys("mima")
+        self.driver.find_element_by_id("TANGRAM__PSP_10__submit").click()
+
+        if self.driver.find_element_by_id("TANGRAM__36__header_h3").text in "安全验证":
+            time.sleep(1)
+            self.driver.find_element_by_id("TANGRAM__36__button_send_mobile").click()
+
+            mss_code = input("请输入短信验证码:")
+            ele_input = self.driver.find_element_by_id("TANGRAM__36__input_vcode")
+
+            ele_input.send_keys(mss_code)
+            self.driver.find_element_by_id("TANGRAM__36__button_submit").click()
+
+        self.verify_and_logout()
+
+    def test_login_with_message(self):
+
+        # 进入短信验证码登录页面
+        self.driver.find_element_by_xpath('//*[@id="u1"]/a[7]').click()
+        ele = self.driver.find_element_by_class_name("pass-form-logo")
+        print(ele.text)
+        if ele.text in "扫码登录":
+            self.driver.find_element_by_id("TANGRAM__PSP_10__footerULoginBtn").click()
+        self.driver.find_element_by_id("TANGRAM__PSP_10__smsSwitchWrapper").click()
+
+        # 验证是否为短信快捷登录
+        ele = self.driver.find_element_by_class_name("pass-form-logo")
+        self.assertIn(ele.text, "短信快捷登录", "当前页面不是短信快捷登录")
+
+        # 输入手机号验证码登录
+        self.driver.find_element_by_id("TANGRAM__PSP_10__smsPhone").send_keys("shoujihao")
+        self.driver.find_element_by_id("TANGRAM__PSP_10__smsTimer").click()
+        message_code = input("请输入短信验证码：")
+        self.driver.find_element_by_id("TANGRAM__PSP_10__smsVerifyCode").send_keys(message_code)
+        self.driver.find_element_by_id("TANGRAM__PSP_10__smsSubmit").click()
+
+        self.verify_and_logout()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- test_case/test_notes.py
+++ test_case/test_notes.py
@@ -11,12 +11,16 @@ def get_ini_data(section, key):
       :param key: get.key =>value
       :return: str
       """
-    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    ini_path = project_path + "/Conf/config.ini"
+    try:
+        # 项目地址
+        project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        ini_path = project_path + "/Conf/config.ini"  # ini文件地址
 
-    conf = configparser.ConfigParser()
-    conf.read(ini_path, encoding="utf-8")
-    return conf.get(section, key)
+        conf = configparser.ConfigParser()  # 定义读取ini文件的对象
+        conf.read(ini_path, encoding="utf-8")  # 读取文件
+        return conf.get(section, key)  # 获取指定value并返回
+    except Exception as err:
+        print(err)
 
 
 class MyTestCase(unittest.TestCase):
@@ -30,17 +34,28 @@ class MyTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
-        pass
 
     def test_maxsize_window(self):  # 浏览器窗口最大化
         # self.driver.set_window_position(0, 0)
         # self.driver.maximize_window()  # 浏览器窗口最大化，mac上无法最大化
         self.driver.set_window_size(1400, 1000)
-        time.sleep(3)
         print("windows尺寸为:", self.driver.get_window_size())  # 打印windows高和宽
         print("缓存为：", self.driver.get_cookies())  # 打印缓存
+        print("缓存list取到第一字典中的value值：", self.driver.get_cookies()[0].get("value"))  # 打印缓存
         print("当前页面title为：", self.driver.title)
         print(self.driver.page_source)
+
+    def test_test1(self):  # 使用ele.text进行断言
+
+        self.driver.find_element_by_class_name("soutu-btn").click()
+        upload_btn = self.driver.find_element_by_class_name("upload-text")
+
+        if "本地上传图片" in upload_btn.text:  # 等价于下面的assertIn断言
+            print("本地上传图片文本已找到")
+        else:
+            print("本地上传图片文本未找到")
+
+        self.assertIn(upload_btn.text, "本地上传图片", "本地上传图片文本未找到")
 
 
 if __name__ == '__main__':
